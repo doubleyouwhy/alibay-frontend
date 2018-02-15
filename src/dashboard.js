43e4df0723358ea52f644a2c3231a7c40539e92a
Megan McEwan <meganmcewan@Megans-MacBook-Pro.local>
2018-02-15 13:14:01 -0500

    dashboard updates

--- src/dashboard.js
+++ src/dashboard.js
@@ -10,45 +10,78 @@ class Dashboard extends Component{
             itemsBought:[]        
         }
     }
+     componentWillMount=()=>{
 
-
-    componentWillMount=()=>{
         fetch ('/itemsForSale?uid=12345')
         .then(x => x.json())
-        .then(y => console.log(y))
+        .then(y =>{
+            console.log(y)
+            var keys = Object.keys(y);
+
+            var itemsForSaleArr = keys.map(element =>{
+                return y[element];
+            })
+            this.setState({ itemsForSale: itemsForSaleArr})
+            })     
 
         fetch('/itemsSold?uid=12345')
         .then(x => x.json())
-        .then(y => console.log(y))
+        .then(y => {
+            console.log(y)
+            var keys = Object.keys(y);
+
+            var itemsSoldArr = keys.map(element=>{
+                return y[element];
+            })
+            this.setState({itemsSold: itemsSoldArr })
+        })
 
         fetch('/itemsBought?uid=12345')
         .then(x => x.json())
-        .then(y => console.log(y))
-    }
-    // dashBoardDrawItem = (element, index, arr) => {
-    //     return    
-    //         <div  className = "product-preview" key={index}>
-    //         <img  className ="thumbnail" alt= {element.prodName} 
-    //               src= {element.image}/>  
+        .then(y => {
+            console.log(y)
+            var keys = Object.keys(y);
 
-    //         <div>{element.prodName}</div>
-    //         <div>{element.price}</div>
-    //     </div>
-    // }
+            var itemsBoughtArr = keys.map(element=>{
+                return y[element];
+            })
+            this.setState({itemsBought: itemsBoughtArr})
+        
+        })
+    }
+
+ 
+    dashBoardDrawItem = (element, index, arr) => {
+        return(    
+            <div  className = "product-preview" key={index}>
+            <img onClick = {()=>this.productPage(element)} 
+                 className ="thumbnail" 
+                 alt= {element.prodName} 
+                 src= {element.image}/>  
+
+            <div>{element.prodName}</div>
+            <div>{element.price}</div>
+         </div>
+            
+        )}
 
     render(){
        
         return (
-             <div id= 'Product'> dashboard
-                    {/* <div className = "gallery"> Items for Sale
+             <div id= 'Product' className = "profile"> 
+                    <h1>Your Profile</h1>
+                    <div>Items you're selling</div>
+                    <div className="profile-gallery"> 
                         {this.state.itemsForSale.map(this.dashBoardDrawItem)}   
                     </div>  
-                    <div className = "gallery"> Sold items
-                        {this.state.itemsSold.map(this.dashboardDrawItem)}   
+                    <div>Items you've sold</div>
+                    <div className="profile-gallery"> 
+                        {this.state.itemsSold.map(this.dashBoardDrawItem)}   
                     </div> 
-                    <div className = "gallery"> Items bought
-                        {this.state.itemsBought.map(this.dashboardDrawItem)}   
-                    </div>        */}
+                    <div>Items you've purchased</div>
+                    <div className="profile-gallery"> 
+                        {this.state.itemsBought.map(this.dashBoardDrawItem)}   
+                    </div>       
             </div>
             )
         }
