ff05b75ba36db28479b1f3865c5bc69907bf1b50
doubleyouwhy <wyendrys@gmail.com>
2018-02-16 18:26:29 -0500

    working on css

--- src/nav.js
+++ src/nav.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 
 class Nav extends Component {
-  constructor() {
+  constructor () {
     super()
     this.state = {
       itemsInCart: 0
@@ -11,24 +11,23 @@ class Nav extends Component {
   // componentWillReceiveProps(nextProps) {
   //   this.setState({ navLandingNoLogin: nextProps.navLandingNoLogin })
   // }
-  render() {
-
+  render () {
     return (
-   
-      <div id="NavBar" >
+
+      <div id='NavBar' >
         <div className='navLogo'>
-        <img src="https://d3ri3cqgw71keo.cloudfront.net/wp-content/uploads/2009/11/LOGO1.gif" onClick={this.props.goToLanding}></img>
+          <img src='https://i.imgur.com/bkc3q8K.png' onClick={this.props.goToLanding} />
         </div>
-          <div className='login-div'>
-       
-          {(this.props.navLandingNoLogin) && <button onClick={this.props.goToLogin} >Log in</button>}
-          {(this.props.navLandingNoLogin) && <button onClick={this.props.goToSignIn} >Sign up</button>}
-          {(this.props.navLandingNoLogin) && <button onClick={this.props.addItemLogin}>ADD ITEM</button>}
-          {(!this.props.navLandingNoLogin) && <button onClick={this.props.addItem}>ADD ITEM</button>}
-          {(!this.props.navLandingNoLogin && this.props.Landing) && <button onClick={this.props.goToProfile}>My Profile</button>}
-          {(!this.props.navLandingNoLogin) && <div className='cart'> Cart {this.state.itemsInCart}</div>}
+        <div className='login-div'>
+
+            {(this.props.navLandingNoLogin) && <button onClick={this.props.goToLogin} >Log in</button>}
+            {(this.props.navLandingNoLogin) && <button onClick={this.props.goToSignIn} >Sign up</button>}
+            {(this.props.navLandingNoLogin) && <button onClick={this.props.addItemLogin}>ADD ITEM</button>}
+            {(!this.props.navLandingNoLogin) && <button onClick={this.props.addItem}>ADD ITEM</button>}
+            {(!this.props.navLandingNoLogin && this.props.Landing) && <button onClick={this.props.goToProfile}>My Profile</button>}
+            {(!this.props.navLandingNoLogin) && <div className='cart'> Cart {this.state.itemsInCart}</div>}
           </div>
-        </div>
+      </div>
 
     )
   }
