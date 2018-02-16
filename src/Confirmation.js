d793ddfe8a55ca72a3daae15ea8654a96b62cb35
doubleyouwhy <wyendrys@gmail.com>
2018-02-15 21:02:26 -0500

    before pull

--- src/Confirmation.js
+++ src/Confirmation.js
@@ -36,7 +36,7 @@ class Confirmation extends Component {
           <h4>Order #{Math.floor(Math.random() * 5000)}</h4>
           <p>A confirmation email has been sent to <a href='/'>{this.props.item.shippingInfo.email}</a></p>
           <h4>Order Placed on: <br/>{fullDate}</h4>
-          <h4>You paid ${(this.props.item.shippingInfo.order.price * 1.15 + 25).toFixed(2)}</h4>
+          <h4>Your total: ${(this.props.item.shippingInfo.order.price * 1.15 + 25).toFixed(2)}</h4>
           <h3>Shipping to</h3>
               <li>{this.props.item.shippingInfo.firstName + ' ' + this.props.item.shippingInfo.lastName}</li>
               <li>{this.props.item.shippingInfo.address}</li>
