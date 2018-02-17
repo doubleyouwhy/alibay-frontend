c9dac6a8238c4746936a60a57c07bd910e157316
Megan McEwan <meganmcewan@Megans-MacBook-Pro.local>
2018-02-17 16:22:49 -0500

    working on sold out

--- src/DrawImage.js
+++ src/DrawImage.js
@@ -2,19 +2,19 @@ import React, { Component } from 'react'
 
 function DrawImage (props) {
   return (
-    <div className='product-preview' >
-        <img
+    <div className='product-preview' > 
+        <div className='img-warpper' >
+            {props.item.isSold?<h2 id='sold-out' >SOLD OUT</h2>:null}
+                <img onClick={() => {props.goToProductPage(props.item) }}
 
-            onClick={() => {
-              props.goToProductPage(props.item)
-            }}
-            className='thumbnail' alt={props.item.prodName}
-            src={props.item.image} />
-
+                    className='thumbnail' alt={props.item.prodName}
+                    src={props.item.image} />
+                
+        </div>
         <div>{props.item.prodName}</div>
         <div>${props.item.price}</div>
     
-        {props.item.isSold?<div>SOLD OUT</div>:null}
+        
     </div>)
 }
 
