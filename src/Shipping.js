addba40501e1bc0a251e92c38f1bdc5ee5120a07
doubleyouwhy <wyendrys@gmail.com>
2018-02-17 11:49:26 -0500

    small changes

--- src/Shipping.js
+++ src/Shipping.js
@@ -64,7 +64,7 @@ class Shipping extends Component {
       })
       .then(z => this.setState({ shippingInfo: { z } }))
 
-    // this.sendEmailConfirmation()
+    this.sendEmailConfirmation()
 
   }
 
